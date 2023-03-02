241442f8d0ea609f96cb7f3fdc5e3ae93561d1be
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2023-03-02 13:36:53 -0300

    Email sender logic

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_l4uccg7",
+        "template_xgzytoi",
+        form.current,
+        "6pf-8URel7n4_0M_R"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+          form.current.reset(); // Clear form here
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  const [done, setDone] = useState(false);
+
   return (
     <section id="contact" className="py-16 lg:section">
       <div className="container mx-auto">
@@ -27,6 +54,8 @@ const Contact = () => {
 
           {/* form */}
           <motion.form
+            ref={form}
+            onSubmit={sendEmail}
             variants={fadeIn("left", 0.3)}
             initial="hidden"
             whileInView={"show"}
@@ -34,20 +63,36 @@ const Contact = () => {
             className="flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 mt-6 mb-20 items-center"
           >
             <input
+              name="user_name"
               className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-[#72ff56] transition-all"
               type="text"
               placeholder="Your name"
+              required
             />
             <input
+              name="user_email"
               className="bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-[#72ff56] transition-all"
-              type="text"
+              type="email"
               placeholder="Your email"
+              required
             />
             <textarea
+              name="message"
               className="bg-transparent border-b py-12 outline-none w-full placeholder:text-white focus:border-[#72ff56] transition-all resize-none mb-12"
               placeholder="Your message"
+              required
             ></textarea>
-            <button className="btn btn-lg">Send message</button>
+            <motion.button
+              type="submit"
+              className="btn btn-lg"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              Send message
+            </motion.button>
+            <span className={`opacity-0 duration-300 ${done && "opacity-100"}`}>
+              I'll be contacting you soon, thank you! :)
+            </span>
           </motion.form>
         </div>
       </div>
