307f9df088b765bc7ac4b9c8238c581532435f0a
luciardrgz <lucia.rodriguez.rosales@gmail.com>
2024-07-07 21:53:47 -0300

    Deleted unused section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 // components
 import Banner from "./components/Banner";
-import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Future from "./components/Future";
 import Present from "./components/Present";
@@ -11,7 +10,6 @@ import Contact from "./components/Contact";
 const App = () => {
   return (
     <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
-      <Header />
       <Banner />
       <Nav />
       <Present />
